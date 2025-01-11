7b8eb2caff550301900c43ad171f5de0e3a79238
Cudjoe94 <cujoefrancis@gmail.com>
2025-01-12 00:36:54 +0100

    new

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Dynamically set the current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Display the last modified date
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
